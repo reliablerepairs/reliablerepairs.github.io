58505fc669972a7b4076192c8ec6cfa00e4630e2
reliablerepairs <reliablerepairsservice@gmail.com>
2025-02-03 12:43:30 -0500

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email recipient
+    $to = "paisleyavg@gmail.com";  // Replace with your email
+
+    // Email subject
+    $subject = "New Contact Form Submission";
+
+    // Email body
+    $body = "You have received a new message from your website contact form.\n\n".
+            "Name: $name\n".
+            "Email: $email\n".
+            "Message:\n$message";
+
+    // Email headers
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "Content-Type: text/plain; charset=UTF-8";
+
+    // Send the email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for contacting us! We'll get back to you shortly.";
+    } else {
+        echo "Sorry, there was an issue sending your message. Please try again later.";
+    }
+}
+?>
